85ce55081cf9d3494a99dd80fcf025633c1b3280
Shingo Ohtsuka <capriciouswalk@gmail.com>
2017-11-10 20:41:34 +0900

    add/use Obstacle class(for player)

--- player/raceState.hpp
+++ player/raceState.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <memory>
+#include <map>
 
 using namespace std;
 
@@ -31,13 +33,52 @@ struct LineSegment {
   bool intersects(const LineSegment &l) const;
 };
 
-enum ObstState { UNKNOWN, OBSTACLE, NONE };
+enum struct ObstState { UNKNOWN, OBSTACLE, NONE };
+
+class ObstacleCol {
+private:
+  using const_iterator = std::vector<ObstState>::const_iterator;
+  int cols;
+  std::vector<ObstState> col;
+public:
+  ObstacleCol() = default;
+  ObstacleCol(ObstState obs, int size);
+  ObstacleCol(const std::vector<int>& arr);
+  ObstState operator[](int pos) const;
+  const_iterator begin() const;
+  const_iterator end() const;
+};
+class Obstacle {
+private:
+  class Obstacle_Impl {
+  private:
+    using const_iterator = std::map<int, ObstacleCol>::const_iterator;
+    const ObstacleCol UNDER;
+    const ObstacleCol UNKNOWN;
+    int rows;
+    std::map<int, ObstacleCol> raw;
+  public:
+    Obstacle_Impl(int width);
+    const ObstacleCol& operator[](int pos) const;
+    const_iterator begin() const;
+    const_iterator end() const;
+    void put(int y, const std::vector<int>& arr);
+  };
+  std::shared_ptr<Obstacle_Impl> obstacle_ptr;
+public:
+  const ObstacleCol& operator[](int pos) const;
+  Obstacle() = default;
+  Obstacle(int width);
+  decltype(obstacle_ptr->begin()) begin() const;
+  decltype(obstacle_ptr->end()) end() const;
+  void put(int y, const std::vector<int>& arr);
+};
 
 struct Course {
   int width, length;
   int vision;
   int thinkTime, stepLimit;
-  vector <vector <ObstState>> obstacle;
+  Obstacle obstacle;
   bool obstacled(const Point &from, const Point &to) const;
   Course(istream &in);
 };
